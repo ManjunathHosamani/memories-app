1169564cac728d957925d3a4b5760eb6c764a820
ManjunathHosamani <manjuhosamani126@gmail.com>
2021-09-03 16:11:44 +0530

    local

--- server/index.js
+++ server/index.js
@@ -15,12 +15,15 @@ app.use(cors());
 app.use("/posts", postRoutes);
 app.use("/user", userRouter);
 
-const CONNECTION_URL =
-  "mongodb+srv://ManjuDB:ManjuDB@cluster0.isnbf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+// const CONNECTION_URL =
+//   "mongodb+srv://ManjuDB:ManjuDB@cluster0.isnbf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() =>
     app.listen(PORT, () =>
       console.log(`Server Running on Port: http://localhost:${PORT}`)
